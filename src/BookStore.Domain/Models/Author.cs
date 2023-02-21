31a91c5bb481e9e1e405c39f135914b808ecbf6e
lparisi1904 <lparisi1904@gmail.com>
2023-02-21 13:16:06 +0100

    Update_20230221

---
+++ src/BookStore.Domain/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStore.Domain.Models;
+
+public partial class Author
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual AuthorContact? AuthorContact { get; set; }
+
+    public virtual ICollection<Book> Books { get; } = new List<Book>();
+}
---
+++ src/BookStore.Domain/Models/Publisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStore.Domain.Models;
+
+public partial class Publisher
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Book> Books { get; } = new List<Book>();
+}
